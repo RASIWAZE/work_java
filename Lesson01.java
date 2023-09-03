038141eb0027f79d66ba50d2a729449198fabac0
RASIWAZE <142482681+RASIWAZE@users.noreply.github.com>
2023-09-03 14:27:28 +0900

    Update Lesson01.java
    インデントを訂正しました

--- Lesson01.java
+++ Lesson01.java
@@ -3,12 +3,11 @@ package work_java;
 public class Lesson01 {
 
 	public static void main(String[] args) {
-		System.out.println("Javaのカリキュラム");
+	System.out.println("Javaのカリキュラム");
 
         System.out.println(120 + 250);
         System.out.println(120 - 250);
         System.out.println(120 * 250);
         System.out.println(120 / 250);
 	}
-
 }
